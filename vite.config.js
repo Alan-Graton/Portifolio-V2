508a0c79b308f19d1674ada788164d4c21d124f5
Alan-Graton <graton.alan@gmail.com>
2023-05-08 20:42:30 -0300

    fix: point vite base config to root

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://alan-graton.github.io/Portifolio-V2/",
+  base: "./",
   plugins: [react()],
 });
