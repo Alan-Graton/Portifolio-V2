1a4f15d0c0f550e2ef6177681a20cc0165b1e6cd
Alan-Graton <graton.alan@gmail.com>
2023-05-12 15:09:18 -0300

    wip-feat: init implementing new structure

---
+++ src/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <>
+      <footer>
+        <h1>I'm the Footer</h1>
+      </footer>
+    </>
+  );
+}
---
+++ src/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Header() {
+    return (
+        <>
+            <header>
+                <h1>I'm the Header</h1>
+            </header>
+        </>
+    )
+}
